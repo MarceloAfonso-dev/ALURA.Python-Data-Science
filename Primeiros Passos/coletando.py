981745267204dc75b9fc2ac5f8cc7e5934bbe42d
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-08-30 01:07:01 -0300

    finalizando aula 2

---
+++ Primeiros Passos/coletando.py
@@ -0,0 +1,29 @@
+nome = input('Qual é o seu nome? ')
+
+ano_entrada = int(input('Escreva o ano de ingresso do(a) estudante: '))
+
+nota_entrada = float(input('Digite a nota do teste de ingresso: '))
+
+print(f'Ano de entrada: {ano_entrada} - nota do teste de ingresso: {nota_entrada}')
+
+#Formatando a saída
+
+nome_aluno = 'Fabricio Daniel'
+idade_aluno = 15
+media_aluno = 8.45
+
+print('Nome do aluno é %s, ele tem %d anos e sua média é %.2f.' %(nome_aluno, idade_aluno, media_aluno))
+
+print('Nome do aluno é {}, ele tem {} anos e sua média é {}.' .format(nome_aluno, idade_aluno, media_aluno))
+
+#Caracteres especiais
+
+print("Estudar é um esforço constante,\nÉ como cultivar uma planta,\nPrecisamos de dedicação e paciência,\nPara ver o fruto amadurecer.")
+
+print('Quantidade\tQualidade\n5 amostras\tAlta\n3 amostras\tBaixa')
+
+print("Caminho do arquivo: C:\\arquivos\\documento.csv")
+
+print("Ouvi uma vez \"Os frutos do conhecimento são os mais doces e duradouros de todos.\"")
+
+print('Minha professora uma vez disse \'Estudar é a chave do sucesso.\' ')
--- Primeiros Passos/strings.py
+++ Primeiros Passos/strings.py
@@ -8,3 +8,9 @@ print(texto.replace('y','t')) #Substitui caracteres
 
 texto = texto.strip().replace('y','t').upper()
 print(texto)
+
+print(chr(64)) #Retorna o caractere correspondente ao código ASCII
+
+print(ord('A')) #Retorna o código ASCII do caractere
+
+print(chr(79) + chr(108) + chr(225)) #Concatenação de caracteres
