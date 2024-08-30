a21d2198ae88b2edc651adcbd6820c7f49ea1f25
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-08-30 00:18:07 -0300

    variáveis

---
+++ Primeiros Passos/variaveis.py
@@ -0,0 +1,14 @@
+idade = 19
+print(idade)
+
+idade = 20
+idade
+
+b = True
+type(b)
+
+nome_aluno = 'John Maverick'
+idade_aluno = 19
+media_aluno = 8.45
+situacao_aprovado = True
+print(nome_aluno, idade_aluno, media_aluno, situacao_aprovado)
