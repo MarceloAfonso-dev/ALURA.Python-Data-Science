5c8fe8b854aaf173415d369f2fa2b0b7cc8de06b
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-09-05 23:04:31 -0300

    feat: condicionais

---
+++ Primeiros Passos/estruturas_condicionais.py
@@ -0,0 +1,9 @@
+media = float(input("Digite a média: "))
+
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
+print("::> Fim do programa")
